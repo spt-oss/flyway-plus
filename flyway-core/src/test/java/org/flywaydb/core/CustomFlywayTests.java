917747b3853b851dd45e24e90c0b57db10aa144c
SPT Developer <spt.developer@gmail.com>
2019-06-21 23:49:32 +0900

    Initial

--- flyway-core/src/test/java/org/flywaydb/core/CustomFlywayTests.java
+++ flyway-core/src/test/java/org/flywaydb/core/CustomFlywayTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.flywaydb.core;
 
 import org.flywaydb.core.api.configuration.FluentConfiguration;
