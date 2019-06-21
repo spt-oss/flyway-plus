42f81c68098e79b50618cfab61ae0c33431a5947
SPT Developer <spt.developer@gmail.com>
2019-06-22 01:57:29 +0900

    Initial

--- flyway-core/src/test/java/org/flywaydb/core/internal/placeholder/MysqlH2SqlReplacerTest.java
+++ flyway-core/src/test/java/org/flywaydb/core/internal/placeholder/MysqlH2SqlReplacerTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.flywaydb.core.internal.placeholder;
 
 import static org.assertj.core.api.Assertions.assertThat;
